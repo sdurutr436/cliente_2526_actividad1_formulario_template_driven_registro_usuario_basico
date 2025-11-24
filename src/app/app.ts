06554deb3f9c834bc1bfd7a743a8e8e9fcd1ca81
sdurutr436 <sdurutr436@g.educaand.es>
2025-11-24 08:14:34 +0100

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('cliente_2526_actividad1_formulario_template_driven_registro_usuario_basico');
+}
